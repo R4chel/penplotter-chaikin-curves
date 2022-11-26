e8fb412017765647107f1fe634ff2835cc635dab
R4chel <rae84@cornell.edu>
2022-11-26 13:47:05 -0500

    first version

--- sketch_chaikin_curves.py
+++ sketch_chaikin_curves.py
@@ -7,7 +7,6 @@ def draw_path(vsk:vsketch.Vsketch, points):
     if len(points) < 2:
         return
     p0 = points[0]
-    p0x, p0y = points[0].x, points[0].y
     for p in points[1:]:
         vsk.line(p0.x,p0.y, p.x,p.y)
         p0 = p
@@ -16,7 +15,7 @@ def draw_path(vsk:vsketch.Vsketch, points):
 class ChaikinCurvesSketch(vsketch.SketchClass):
     # Sketch parameters:
     num_points = vsketch.Param(5)
-    # iterations = vsketch.Param(7)
+    iterations = vsketch.Param(7)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a6", landscape=True)
@@ -26,7 +25,18 @@ class ChaikinCurvesSketch(vsketch.SketchClass):
         width, height = factor * vsk.width, factor * vsk.height
 
         points = [Point(vsk.random(width), vsk.random(height)) for _ in range(self.num_points)]
+        points.sort(key=lambda p : p.x)
         draw_path(vsk, points)
+
+        for _ in range(self.iterations):
+            new_points = []
+            p0 = points[0]
+            for p in points[1:]:
+                new_points.append(Point(p0.x*.75+p.x*.25,p0.y*.75+p.y*.25))
+                new_points.append(Point(p0.x*.25+p.x*.75,p0.y*.25+p.y*.75))
+                p0 = p
+            draw_path(vsk, new_points)
+            points = new_points
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
