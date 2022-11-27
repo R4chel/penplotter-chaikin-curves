206d20e11596cdebd0c481d2b8aa4e790bf8a4c9
R4chel <rae84@cornell.edu>
2022-11-27 15:57:18 -0500

    reformat

--- sketch_chaikin_curves.py
+++ sketch_chaikin_curves.py
@@ -3,22 +3,23 @@ from shapely.geometry import Point
 import math
 import vpype as vp
 
+
 def to_cartesian(r, theta):
     return Point(r * math.cos(theta), r * math.sin(theta))
 
 
-
 def to_polar(origin, p):
     r = p.distance(origin)
-    theta = math.acos((p.x-origin.x)/r)
+    theta = math.acos((p.x - origin.x) / r)
     return (r, theta)
+
 
 class ChaikinCurvesSketch(vsketch.SketchClass):
     # Sketch parameters:
     num_points = vsketch.Param(5)
     iterations = vsketch.Param(4)
     closed = vsketch.Param(False)
-    debug= vsketch.Param(False)
+    debug = vsketch.Param(False)
     only_draw_last = vsketch.Param(False)
     always_resort = vsketch.Param(False)
     precision = vsketch.Param(3)
@@ -26,38 +27,52 @@ class ChaikinCurvesSketch(vsketch.SketchClass):
     def generate_points(self, vsk: vsketch.Vsketch):
         points = []
         if self.closed:
-            thetas = [vsk.random(2*math.pi) for _ in range(self.num_points)]
+            thetas = [vsk.random(2 * math.pi) for _ in range(self.num_points)]
             thetas.sort()
             # This only works if origin = center of page
-            polar_points = [(vsk.random(0, min(abs(self.width/(2*math.cos(theta))),abs(self.height/(2*math.sin(theta))))), theta) for theta in thetas]
-            points = [Point(r * math.cos(theta) + self.origin.x, r * math.sin(theta) + self.origin.y) for (r, theta) in polar_points]
+            polar_points = [(vsk.random(
+                0,
+                min(abs(self.width / (2 * math.cos(theta))),
+                    abs(self.height / (2 * math.sin(theta))))), theta)
+                            for theta in thetas]
+            points = [
+                Point(r * math.cos(theta) + self.origin.x,
+                      r * math.sin(theta) + self.origin.y)
+                for (r, theta) in polar_points
+            ]
             points.append(points[0])
             if self.debug:
                 vsk.stroke(2)
                 for p in points:
-                    vsk.line(self.origin.x, self.origin.y, p.x,p.y)
+                    vsk.line(self.origin.x, self.origin.y, p.x, p.y)
                 vsk.stroke(1)
         else:
-            xs = [round(vsk.random(self.width),self.precision) for _ in range(self.num_points)]
+            xs = [
+                round(vsk.random(self.width), self.precision)
+                for _ in range(self.num_points)
+            ]
             xs.sort()
-            points = [Point(x, vsk.random(self.height))for x in xs]
-        return [Point(round(point.x, self.precision), round(point.y, self.precision)) for point in points]
+            points = [Point(x, vsk.random(self.height)) for x in xs]
+        return [
+            Point(round(point.x, self.precision),
+                  round(point.y, self.precision)) for point in points
+        ]
 
     def sort_points(self, points):
         if self.closed:
             # self.center = Point(sum([p.x for p in points])/len(points), sum([p.y for p in points])/len(points))
-            points.sort(key = lambda p : to_polar(self.origin, p)[::-1])
+            points.sort(key=lambda p: to_polar(self.origin, p)[::-1])
         else:
-            points.sort(key = lambda p : ( p.x, p.y ))
+            points.sort(key=lambda p: (p.x, p.y))
         if self.closed and points[-1] != points[0]:
             points.append(points[0])
 
     def lerp_points(self, p0, p1, pct):
-        x = round(p0.x*pct+p1.x*(1-pct), self.precision)
-        y = round(p0.y*pct+p1.y*(1-pct), self.precision)
-        return Point(x,y)
+        x = round(p0.x * pct + p1.x * (1 - pct), self.precision)
+        y = round(p0.y * pct + p1.y * (1 - pct), self.precision)
+        return Point(x, y)
 
-    def draw_path(self, vsk:vsketch.Vsketch, points):
+    def draw_path(self, vsk: vsketch.Vsketch, points):
         if len(points) < 2:
             return
         p0 = points[0]
@@ -67,11 +82,10 @@ class ChaikinCurvesSketch(vsketch.SketchClass):
                 vsk.strokeWeight(i)
                 i = i % 10
                 i += 1
-            vsk.line(p0.x,p0.y, p.x,p.y)
+            vsk.line(p0.x, p0.y, p.x, p.y)
             p0 = p
         # if closed:
         #     vsk.line(p0.x, p0.y, points[0].x,points[0].y)
-
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("a6", landscape=True)
@@ -79,15 +93,15 @@ class ChaikinCurvesSketch(vsketch.SketchClass):
         vsk.scale(scale)
         factor = 1 / vp.convert_length(scale)
         self.width, self.height = factor * vsk.width, factor * vsk.height
-        self.origin = Point(self.width/2, self.height/2)
+        self.origin = Point(self.width / 2, self.height / 2)
 
         points = self.generate_points(vsk)
         if self.debug:
-            vsk.circle(self.origin.x,self.origin.y, 20)
+            vsk.circle(self.origin.x, self.origin.y, 20)
             print([(p.x, p.y) for p in points])
         if not self.only_draw_last:
             self.draw_path(vsk, points)
-         
+
         for _ in range(self.iterations):
             new_points = []
             p0 = points[0]
@@ -99,10 +113,10 @@ class ChaikinCurvesSketch(vsketch.SketchClass):
                 if lerp2 != new_points[-1]:
                     new_points.append(lerp2)
                 p0 = p
-            
+
             if not self.only_draw_last:
                 self.draw_path(vsk, points)
-         
+
             points = new_points
             if self.always_resort:
                 self.sort_points(points)
